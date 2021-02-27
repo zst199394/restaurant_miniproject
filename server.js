42e58d5984b4c43c2ea8650eb2d6eed6eae104c7
Richie Roman <answer417@gmail.com>
2021-02-27 11:07:36 -0500

    server update

--- server.js
+++ server.js
@@ -4,17 +4,44 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-let currentRes = [];
-let waitingList = [];
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'home.html')))
+let tables = [];
+let waitlist = [];
 
-app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, 'tables.html')))
 
-app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, 'reserve.html')))
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
 
-app.get('/api/tables', (req,res) => res.join(currentRes));
+app.get('/tables', function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
 
-app.get('/api/waitlist', (req, res) => res.join(waitingList));
+app.get('/reserve', function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+});
 
-app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+app.get('/api/tables', function(req, res) {
+    res.json(tables);
+});
+
+app.get('/api/waitlist', function(req, res) {
+    res.json(waitlist);
+});
+
+app.post('/api/tables', function(req, res) {
+    if (tables.length < 5) {
+        tables.push(req.body);
+        res.send("tables");
+    } else {
+        waitlist.push(req.body)
+        res.send("waitlist");
+    }
+
+})
+
+app.listen(PORT, function() {
+    console.log(`Listening on Port ${PORT}`);
+});
